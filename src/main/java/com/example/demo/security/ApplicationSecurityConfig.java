ea0a4e086f1fc9aa860671a3dc1c60741bdfb947
Ada <ada.grzegorczyk@gmail.com>
2022-10-19 20:39:40 +0200

    CSRF TOKEN

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -4,6 +4,8 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,11 +15,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.demo.security.ApplicationUserRole.ADMIN;
-import static com.example.demo.security.ApplicationUserRole.STUDENT;
+import static com.example.demo.security.ApplicationUserRole.*;
 
 @EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -33,7 +35,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
        http
                .authorizeRequests()
-               .and().authorizeRequests()
                .antMatchers("/","index","/css/", "/js/*")
                .permitAll()
                .antMatchers("/api/**").hasRole(STUDENT.name())
@@ -51,20 +52,31 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+  //              .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthority())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("Linka")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+     //           .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthority())
                 .build();
 
 
 
+        UserDetails tomUser = User.builder()
+                .username("Tom")
+                .password(passwordEncoder.encode("password"))
+      //          .roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthority())
+                .build();
+
+
         return  new InMemoryUserDetailsManager(
                 annaSmithUser,
-                lindaUser
+                lindaUser,
+                tomUser
         );
     }
 }
--- src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -1,13 +1,18 @@
 package com.example.demo.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.example.demo.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
 
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(ApplicationUserPermission.COURSE_READ, ApplicationUserPermission.COURSE_WRITE));
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_WRITE, STUDENT_READ)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
 
     private final Set<ApplicationUserPermission> permissions;
@@ -19,5 +24,17 @@ public enum ApplicationUserRole {
 
     public Set<ApplicationUserPermission> getPermissions() {
         return permissions;
+    }
+
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthority (){
+        Set<SimpleGrantedAuthority> permissions = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+ this.name()));
+        return permissions;
+
     }
 }
