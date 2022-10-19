a66a5b86fe32756e743586ef47d330bb89b1a762
Ada <ada.grzegorczyk@gmail.com>
2022-10-19 21:21:26 +0200

    CSRF Added http.csrf() method to access CSRF token.

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
@@ -34,6 +35,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http
+               .csrf()
+               .and()
                .authorizeRequests()
                .antMatchers("/","index","/css/", "/js/*")
                .permitAll()
